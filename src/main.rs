664febb81b8e3d5760e90658dfc9f0b0d19096db
S0ra <s0ra@duck.com>
2025-10-15 23:02:19 -0500

    Add a custom stationary icon support Add default values for everything and transform everything into --args

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,20 @@ use substring::Substring;
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Length of the outputed value
+    #[arg(short, long, default_value_t = 20)]
     length: usize,
+
     /// ms of time to wait between outputs
+    #[arg(short, long, default_value_t = 300)]
     delay: u64,
+
     /// ms of time to wait before scrolling
+    #[arg(short, long, default_value_t = 300)]
     wait_time: u64,
+
+    /// custom icon
+    #[arg(short, long, default_value_t = String::from("\u{f1bc} "))]
+    custom_icon: String,
 }
 
 fn main() -> ! {
@@ -20,9 +29,9 @@ fn main() -> ! {
     let mut index = 0;
     loop {
         string = update_string(&string);
-        print_string(&string, index, cli.length);
+        print_string(&string, index, cli.length, &cli.custom_icon);
         thread::sleep(time::Duration::from_millis(cli.delay));
-        index = index + 1;
+        index += 1;
         if index > string.len() + 3 {
             thread::sleep(time::Duration::from_millis(cli.wait_time));
             index = 1;
@@ -33,19 +42,22 @@ fn main() -> ! {
 fn update_string(last_string: &str) -> String {
     let metadata = PlayerCtl::metadata();
     let new_string = metadata.artist + " - " + &metadata.title;
-    println!("{}", &new_string);
+    //println!("{}", &new_string);
     if last_string != new_string {
-        return new_string;
+        new_string
     } else {
-        return last_string.to_string();
+        last_string.to_string()
     }
 }
 
-fn print_string(string: &str, index: usize, length: usize) {
+fn print_string(string: &str, index: usize, length: usize, custom_icon: &str) {
     if string.len() > length {
         let scroll_substring = string.to_owned() + " | " + string;
-        println!("{}", &scroll_substring.substring(index, index + length))
+        println!(
+            "{custom_icon}{}",
+            &scroll_substring.substring(index, index + length)
+        )
     } else {
-        println!("{}", &string);
+        println!("{custom_icon}{}", &string);
     }
 }
