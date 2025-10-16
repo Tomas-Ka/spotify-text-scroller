402ab973d07b8dccb5cb349b90012358c9e61c1c
S0ra <s0ra@duck.com>
2025-10-15 23:16:16 -0500

    Refactor main.rs: clean imports, fix docs, and adjust scrolling string handling

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,47 @@
 use clap::Parser;
-use playerctl::PlayerCtl;
-use std::{thread, time};
+use playerctl::{PlayerCtl, TrackMetadata};
+use std::{thread::sleep, time::Duration};
 use substring::Substring;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Length of the outputed value
+    /// Length of the output value
     #[arg(short, long, default_value_t = 20)]
     length: usize,
 
-    /// ms of time to wait between outputs
+    /// Time to wait between outputs in ms
     #[arg(short, long, default_value_t = 300)]
     delay: u64,
 
-    /// ms of time to wait before scrolling
+    /// Time to wait before scrolling in ms
     #[arg(short, long, default_value_t = 300)]
     wait_time: u64,
 
-    /// custom icon
+    /// Custom icon
     #[arg(short, long, default_value_t = String::from("\u{f1bc} "))]
     custom_icon: String,
 }
 
 fn main() -> ! {
     let cli = Cli::parse();
-    let mut string: String = "this is a test".to_string();
+    let mut string: String = String::new();
     let mut index = 0;
     loop {
         string = update_string(&string);
         print_string(&string, index, cli.length, &cli.custom_icon);
-        thread::sleep(time::Duration::from_millis(cli.delay));
+        sleep(Duration::from_millis(cli.delay));
         index += 1;
         if index > string.len() + 3 {
-            thread::sleep(time::Duration::from_millis(cli.wait_time));
+            sleep(Duration::from_millis(cli.wait_time));
             index = 1;
         }
     }
 }
 
 fn update_string(last_string: &str) -> String {
-    let metadata = PlayerCtl::metadata();
-    let new_string = metadata.artist + " - " + &metadata.title;
-    //println!("{}", &new_string);
+    let TrackMetadata { artist, title, .. } = PlayerCtl::metadata();
+    let new_string = format!("{artist} - {title}");
     if last_string != new_string {
         new_string
     } else {
